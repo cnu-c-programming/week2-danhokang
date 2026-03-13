cdb18ca839225a848f669327a7c8c7501390022c
danhokang <danho2898@naver.com>
2026-03-13 10:11:26 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -10,21 +10,18 @@ int main()
     if (n < 2) {
         isPrime = 0;
     }
-    else {
-        for (i = 2; i < n; i++) {
-            if (n % i == 0) {
-                isPrime = 0;
-                break;
-            }
+
+    for (i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
         }
     }
 
-    if (isPrime) {
-        printf("true\n");
-    }
-    else {
-        printf("false\n");
-    }
+    if (isPrime)
+        printf("true");
+    else
+        printf("false");
 
     return 0;
 }
