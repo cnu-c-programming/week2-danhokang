f01089538ad5f4a9b91161ebc3e198db256d4cd9
danhokang <danho2898@naver.com>
2026-03-13 09:26:58 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    printf("int : %d\n", 100);
+    printf("hex : %x\n", 100);
+    printf("oct : %o\n", 100);
+    printf("char : %c\n", 'c');
+    printf("string : %s\n", "hello world!");
+    printf("float : %f\n", 3.141592);
+    printf("exp : %e\n", 3.141592);
 
     return 0;
 }
