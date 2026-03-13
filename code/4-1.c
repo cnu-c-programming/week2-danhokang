ef5624064e7a0be7f8563e1651a6f5e79c876076
danhokang <danho2898@naver.com>
2026-03-13 10:06:04 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    if (op == '+')
+        printf("%d\n", a + b);
+    else if (op == '-')
+        printf("%d\n", a - b);
+    else if (op == '*')
+        printf("%d\n", a * b);
+    else if (op == '/')
+        printf("%d\n", a / b);
 
     return 0;
 }
